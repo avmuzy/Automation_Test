8bd60e05f12aca2ba3f59230614344940d56496f
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-15 23:10:35 -0300

    Spotify & Firefox

--- Spotify test.py
+++ Spotify test.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 pyautogui.alert('Eh hora de Rock')
 pyautogui.PAUSE = 0.5
-pyautogui.press('winleft')
+pyautogui.press('startp')
 time.sleep(1)
 pyautogui.write('Spotify app')
 pyautogui.press('enter')
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import time
 #alert message
 pyautogui.alert("Vamos dar bom dia!")
 pyautogui.PAUSE = 0.5
-
 pyautogui.press('winleft')
 pyautogui.write('Firefox')
 pyautogui.press('enter')
@@ -11,4 +10,5 @@ time.sleep(10)
 pyautogui.write('https://discord.com/channels/821791958744957028/902631152886636574')
 time.sleep(5)
 pyautogui.press('enter')
+
 

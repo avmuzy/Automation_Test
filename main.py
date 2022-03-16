ccdf09451ec61053283dcb791ef2aad70a7003ef
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-16 20:21:44 -0300

    add message & discord channel set

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ pyautogui.PAUSE = 0.5
 pyautogui.press('winleft')
 pyautogui.write('Firefox')
 pyautogui.press('enter')
-time.sleep(10)
-pyautogui.write('https://discord.com/channels/821791958744957028/902631152886636574')
+time.sleep(20)
+pyautogui.write('https://discord.com/channels/821791958744957028/938486939382874172')
 time.sleep(5)
 pyautogui.press('enter')
+time.sleep(15)
+pyautogui.write('Boa noite grupo 12. ')
+pyautogui.write(' Essa mensagem foi escrita usando pyautogui')
+pyautogui.press('enter')
+
+
 
 

4a3892c3d48ab93674c1bbaa77ac1dbd9fe7ddda
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-15 22:56:25 -0300

    Spotify test

--- Spotify test.py
+++ Spotify test.py
@@ -3,4 +3,6 @@ import time
 pyautogui.alert('Eh hora de Rock')
 pyautogui.PAUSE = 0.5
 pyautogui.press('winleft')
-pyautogui.write('Spotfy')
+time.sleep(1)
+pyautogui.write('Spotify app')
+pyautogui.press('enter')
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pyautogui
 import time
 #alert message
-pyautogui.alert("Vamos abrir o Chrome!")
+pyautogui.alert("Vamos dar bom dia!")
 pyautogui.PAUSE = 0.5
 
 pyautogui.press('winleft')
-pyautogui.write('chrome')
+pyautogui.write('Firefox')
 pyautogui.press('enter')
-time.sleep(1)
-pyautogui.write('https://mail.google.com/')
+time.sleep(10)
+pyautogui.write('https://discord.com/channels/821791958744957028/902631152886636574')
+time.sleep(5)
+pyautogui.press('enter')
 
